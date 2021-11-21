4e9afe622ef86187556d5463c55c02f37936c959
Gabriel Hammer <gabriel.hammer523@gmail.com>
2021-11-20 21:20:23 -0300

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-system-bar
+      app
+      height="20"
+      class="d-flex justify-center"
+      >
+      v-system-bar 
+      </v-system-bar>
+    <v-app-bar
+      app
+      elevation="4"
+      class="d-flex justify-center"
+    >
+     v-app-bar
+    </v-app-bar>
+
+    <v-main>
+
+
+        <v-row style="height: 100%; margin: 0">
+            <v-navigation-drawer 
+              permanent
+              class="d-flex align-center justify-center"
+            >
+              <div style="height: 100%" class="d-flex align-center">v-navigation-drawer</div>
+            </v-navigation-drawer>
+            <v-content style="padding: 0">
+              <div 
+                style="height: 100%"
+                class="d-flex align-center justify-center"
+              >v-content</div>
+            </v-content>
+        
+        </v-row>
+
+
+    </v-main>
+
+    <v-footer
+      class="d-flex justify-center"
+    >v-footer</v-footer>
+    <v-bottom-navigation
+      class="d-flex align-center"
+    >v-bottom-navigation</v-bottom-navigation>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
